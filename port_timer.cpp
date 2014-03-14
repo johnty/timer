8bdfa7626a13f71b8070acb510e4e208824af093
johnty <johntywang@gmail.com>
2014-03-14 16:01:32 -0700

    better cmdline options

--- port_timer.cpp
+++ port_timer.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <stdlib.h>
 
 using namespace std;
 using namespace yarp::os;
@@ -11,33 +12,53 @@ int main(int argc, char *argv[]) {
     BufferedPort<Bottle> port;
 
     string pname = "/timer";
-    if (argc == 2) {
+    double t = 0;
+    bool verbose = false;
+    
+    if (argc < 3) {
+        cout <<"usage: port_timer [/port_name] [t (seconds)] [-v )optional]"<<endl;
+        return 0;
+    }
+    
+    else { //ok, lets go on...
         pname = string(argv[1]);
-        
+        t = atof(argv[2]);
+        if (argc == 4) {
+            verbose = true; // ha ha hack
+        }
     }
     port.open(pname.c_str());
-    cout << "waiting for input" << endl;
+    cout << "Port Timer set up for " <<t << "s intervals";
+    if (verbose)
+        cout <<"; verbose=ON"<<endl;
+    else
+        cout <<endl;
     
+    //set clock start
     std::clock_t start;
     double duration;
     start = std::clock();
     long count = 0;
     while (true) {
+        //find current time;
         duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
         if (port.getPendingReads() > 0) {
             Bottle *input = port.read();
             if (input!=NULL) {
                 count++;
-                cout << "got " << input->toString().c_str() << endl;
+                if (verbose)
+                    cout << "got " << input->toString().c_str() << endl;
                 //double total = 0;
                 //for (int i=0; i<input->size(); i++) {
                 //    total += input->get(i).asDouble();
                 //}
             }
         }
+        
+        //if reached polling interval, reset count + measure FPS
         if (duration >= 2.0) {
             start = std::clock();
-            cout <<"received " << count << " frames in 2 seconds; FPS = "<<count/2.0 <<endl<<endl;
+            cout <<"received " << count << " frames in "<< t <<" seconds; FPS = "<<count/t <<endl<<endl;
             count = 0; //reset counter
             
         }
