3c448bd8fde4c5377b9143b63c16c359707d7a87
johnty <johntywang@gmail.com>
2014-03-18 12:23:10 -0700

    added send script and gitignore

---
+++ send_script.sh
@@ -0,0 +1,57 @@
+echo starting sender1
+./simple_sender /s1 /timer &
+sleep 8
+
+echo starting sender2
+./simple_sender /s2 /timer &
+sleep 8
+
+echo starting sender3
+./simple_sender /s3 /timer &
+sleep 8
+
+echo starting sender4
+./simple_sender /s4 /timer &
+sleep 8
+
+echo starting sender5
+./simple_sender /s5 /timer &
+sleep 8
+
+echo starting sender6
+./simple_sender /s6 /timer &
+sleep 8
+
+echo starting sender7
+./simple_sender /s7 /timer &
+sleep 8
+
+echo starting sender8
+./simple_sender /s8 /timer &
+sleep 8
+
+echo starting sender9
+./simple_sender /s9 /timer &
+sleep 8
+
+echo starting sender10
+./simple_sender /s10 /timer &
+sleep 8
+
+echo starting sender11
+./simple_sender /s11 /timer &
+sleep 8
+
+echo starting sender12
+./simple_sender /s12 /timer &
+sleep 8 
+
+echo starting sender13
+./simple_sender /s13 /timer &
+sleep 8 
+
+echo starting sender14
+./simple_sender /s14 /timer &
+sleep 8 
+
+echo done!
